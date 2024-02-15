7d63028f1008f45e059c95287ca86c8f4b9fc51a
Sarah Li <100035287+sli051357@users.noreply.github.com>
2024-02-15 13:33:55 -0800

    guys i'm so cool (that is I finished most of it up)

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,46 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# Detect whether file exists
+file=`find student-submission -name "ListExamples.java"`
+if [ -f $file ]; then 
+   echo "File found"
+else 
+   echo "File ListExamples.java not found"
+   exit 1
+fi 
+
+# What if list-examples is in a directory by itself?
+
+# Copy files into grading-area
+cp $file grading-area/
+cp TestListExamples.java grading-area/ 
+cp -r lib/ grading-area/
+
+cd grading-area
+javac -cp "$CPATH" *.java
+
+if [ $? -ne 0 ]; then
+    echo "Compilation error!"
+    exit 1
+fi
+
+echo "Program compiled successfully!"
+
+# actually run the program and report the grade
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+if grep -q FAILURES "junit-output.txt"; then 
+    lastline=$( tail -n 2 junit-output.txt )
+    totaltests=$( awk -F ': |,' '{print $2}' <<< $lastline )
+    failures=$(awk -F ': |,' '{print $4}' <<< $lastline )
+    successes=$((totaltests - failures))
+else 
+    lastline=$( tail -n 2 junit-output.txt )
+    totaltests=$( awk -F '\\(| ' '{print $3}' <<< $lastline )
+    successes=$totaltests
+fi
+echo "Your score is $successes / $totaltests"
+
+exit 0
